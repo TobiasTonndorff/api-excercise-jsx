5d7cfd1b2fd27f878b5ab98c47cc1e86db6322b4
Tobias Stenbo Tonndorff <tobias.tonndorff@cph.dk>
2023-11-24 05:10:48 +0100

    prettier

--- src/components/PersonList.jsx
+++ src/components/PersonList.jsx
@@ -1,43 +1,39 @@
-function PersonList({persons, deletePersonById}) {
-
-
-    
-    return ( 
+function PersonList({ persons, deletePersonById }) {
+  return (
     <div>
-        <h1>Person list</h1>
-        
-        
-    <table className="table table-striped">
+      <h1>Person list</h1>
+
+      <table className="table table-striped">
         <thead>
-        <tr>
+          <tr>
             <th>Id</th>
             <th>Name</th>
             <th>Age</th>
             <th>Email</th>
             <th>Gender</th>
             <th>Action</th>
-        </tr>
-    </thead>
-    <tbody>
-        {persons.map((person) => (
-                     <tr key={crypto.randomUUID()}>
-                     <td>{person.id}</td>
-                     <td>{person.name}</td>
-                     <td>{person.age}</td>
-                     <td>{person.email}</td>
-                     <td>{person.gender}</td>
-                     <td>
-                         <button>Edit</button>
-                         <button onClick={() => deletePersonById(person.id)}>Delete</button>
-                     </td>
-                     </tr>
-            
-                
-        ))}
-    </tbody>
-    </table>
-    </div> 
-    );
+          </tr>
+        </thead>
+        <tbody>
+          {persons.map((person) => (
+            <tr key={crypto.randomUUID()}>
+              <td>{person.id}</td>
+              <td>{person.name}</td>
+              <td>{person.age}</td>
+              <td>{person.email}</td>
+              <td>{person.gender}</td>
+              <td>
+                <button>Edit</button>
+                <button onClick={() => deletePersonById(person.id)}>
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default PersonList;
